3ac4f9615bccdb44597f05fa1f7e299a78fa97b7
KriStar88 <kristina_arapova@mail.ru>
2024-08-08 21:13:04 +0300

    Обработка исключений

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
+from tkinter import messagebox as mb
 import requests
 
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {"file": open(filepath, 'rb')}
-        response = requests.post("http://file.io", files=files)
-        if response.status_code == 200:
-            link = response.json()['link']
-            entry.insert(0,link)
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {"file": f}
+                response = requests.post("http://file.io", files=files)
+                response.raise_for_status()
+                link = response.json().get('link')
+                if link:
+                    entry.delete(0, END)
+                    entry.insert(0, link)
+                else:
+                    raise ValueError("Не удалось получить ссылку")
+    except Exception as e:
+        mb.showerror("Ошибка", f"произошла ошибка: {e}")
+
 
 window = Tk()
 window.title("Сохранение файла в облаке")
 window.geometry("400x300")
-
 button = ttk.Button(text="Загрузить файл", command=upload)
 button.pack()
-
 entry = ttk.Entry()
 entry.pack()
 
